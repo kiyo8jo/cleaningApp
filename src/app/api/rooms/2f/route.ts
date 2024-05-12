439ac6cee485822411f322d30ba0885f68dc3d6b
Taiki Kiyoshiro <hoxq4387@yahoo.co.jp>
2024-05-12 18:44:43 +0900

    q

--- src/app/api/rooms/2f/route.ts
+++ src/app/api/rooms/2f/route.ts
@@ -1,6 +1,9 @@
 import { supabase } from "@/utils/supabaseClient";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
+
 export async function GET(req: NextRequest) {
   let { data, error } = await supabase.from("rooms_2f").select("*").order("id");
   if (error) {
--- src/app/front/page.tsx
+++ src/app/front/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Main from "../../../features/main/components/Main/Main";
 
-export const dynamic = 'force-dynamic';
-
 const page = () => {
   return <Main />;
 };
